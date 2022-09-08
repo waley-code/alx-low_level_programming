395aa2d09de8e4c34307469c6d5201e566846f7b
Adeosun Oyewale <walephlp@gmail.com>
2022-09-08 12:02:38 -0700

    Prints a sentence

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ /*
+  * Prints \"Programming is like building a multilingual puzzle
+  * main - The entry point
+  */
+int main(void)
+{
+	/*Prints \"Programming is like building a multilingual puzzle */
+	puts("\"Programming is like building a multilingual puzzle\n");
+	/*retuns 0 to main*/
+	return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -11,3 +11,6 @@
 
 ### 3-name 
 	This is a script that compiles a C file and creates an executable named cisfun.
+
+### 4-puts.c
+	This creates a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
