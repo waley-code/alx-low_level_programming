27dee984c5089248fda41fc96b569b2c4345db4f
Adeosun Oyewale <walephlp@gmail.com>
2022-09-13 11:54:48 -0700

    Funtions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - main function
+* Description: 'The main function'
+* return: returns 0
+*
+* _putchar: prints character
+*/
+
+int main(void)/*Description: 'The main function'*/
+{
+	printf("_putchar\n"); /*Prints _putchar followed by a line*/
+
+	return (0);/*return 0*/
+
+} /*end of function main*/
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,6 @@
+# C - Functions, nested loops
+
+
+### 0. _putchar
+
+	A program that prints _putchar, followed by a new line.
