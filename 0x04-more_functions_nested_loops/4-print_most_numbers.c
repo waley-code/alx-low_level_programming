688adc7e5faf00d53f07f0b4cb6918430bf4ec69
Adeosun Oyewale <walephlp@gmail.com>
2022-09-15 14:58:38 -0700

    print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_numbers - Function that loops through digits excepts 2 and 4
+* _putchar - Prints characters
+* Return: Returns 0
+*/
+void print_numbers(void)
+{
+	char num = 48;
+
+	while (num >= 48 && num <= 57)
+	{
+		if (!(num == 50 || num == 52))
+			_putchar(num);
+		num++;
+	}
+	_putchar('\n');
+}
