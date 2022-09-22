da43f7d1697a4213f3e7abe5208a93bdb0091856
Adeosun Oyewale <walephlp@gmail.com>
2022-09-22 10:03:11 -0700

    Pointers

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,10 +2,10 @@
 #include <string.h>
 /**
 * _strncpy - Main function
-* @dest : Pointer to char datatype
-* @src : Pointer pointing to char datatype
+* @dest : char parameter
+* @src : Pointer pointing to argument of char datatype
 * @n : Argument of int datatype
-* Return: dest
+* Return: Returns dest
 */
 char *_strncpy(char *dest, char *src, int n)
 {
