f4e1d48236807111ef8678c096d3929c4888f133
Adeosun Oyewale <walephlp@gmail.com>
2022-09-22 17:12:44 -0700

    Pointers

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * leet - Fyunction that encodes a string into 1337
  * @s: A pointer pointing to addrss of a string to encode
@@ -8,8 +7,11 @@
 char *leet(char *s)
 {
 	int c = 0;
+
 	int d = 0;
+
 	char a[] = "aAeEoOtTlL";
+
 	char b[] = "4433007711";
 
 	while (*(s + c))
