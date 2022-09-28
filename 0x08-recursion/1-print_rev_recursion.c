c48936cd6eccc1b8c1be4f8e2c888a77f069f5d4
Adeosun Oyewale <walephlp@gmail.com>
2022-09-28 11:21:31 -0700

    Rev_recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+/**
+*  _print_rev_recursion - Main function that gets called over
+* @s: Pointer argument pointing to a char datatype
+* Return: Returns nothing
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_puts_recursion(s + 1);
+	_putchar(*s);
+
+
+}
