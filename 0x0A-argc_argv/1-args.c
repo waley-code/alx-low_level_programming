32cbb6c4101cc3d51a3f95c2e7620526209c5e0e
Adeosun Oyewale <walephlp@gmail.com>
2022-09-30 16:11:26 -0700

    argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* main - Main function
+* @argc: first argument of the main function
+* @argv: second argument of the main function
+* Return: Returns 0 always
+*/
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* main - Main function
+* @argc: first argument of the main function
+* @argv: second argument of the main function
+* Return: Returns 0 always
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i <= argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
