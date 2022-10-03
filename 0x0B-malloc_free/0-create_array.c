f21b0315fcba69c3c46cbe85c7d421313cef2d44
Adeosun Oyewale <walephlp@gmail.com>
2022-10-03 09:06:00 -0700

    Malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* create_array - Main function or Entry point
+* @size: Argument indicating size of an inputed array
+* @c: Argument of char datatype
+* Return: Returns a pointer pointing to created array / NULL when it fails
+*/
+char *create_array(unsigned int size, char c)
+{
+	char *a = NULL;
+
+	unsigned int b = 0;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		a = (char *)malloc(size * sizeof(char));
+		if (a != NULL)
+		{
+			while (b < size)
+			{
+				array[b] = c;
+				b++;
+			}
+		}
+	}
+	return (a);
+}
