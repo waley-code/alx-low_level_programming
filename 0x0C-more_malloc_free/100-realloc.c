93558f39dfae7baf4e35d6e0474e2a661405060a
Adeosun Oyewale <walephlp@gmail.com>
2022-10-07 16:32:32 -0700

    malloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+* _realloc - Main function that reallocates memory sizes
+* @ptr: A pointer argument pointing to an address of old location
+* @old_size: Argument of unsigned int datatype of old memory size
+* @new_size: Argument of unsigned int datatype for new memory size
+* Return:  Rreturns a variable pointing to array
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *str;
+
+	if (new_size > old_size)
+		str = malloc(new_size);
+		free(ptr);
+		return (str);
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+		s = malloc(new_size);
+		free(ptr);
+		return (str);
+	if (new_size == 0 && ptr != NULL)
+		free(ptr);
+		return (NULL);
+	return (ptr);
+}
