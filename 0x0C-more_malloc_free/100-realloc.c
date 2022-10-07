1c2dc5823262df61372e9a4d97533f1479fa7c94
Adeosun Oyewale <walephlp@gmail.com>
2022-10-07 16:37:05 -0700

    malloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,17 +14,25 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *str;
 
 	if (new_size > old_size)
+	{
 		str = malloc(new_size);
 		free(ptr);
 		return (str);
+	}
 	if (new_size == old_size)
+	{
 		return (ptr);
+	}
 	if (ptr == NULL)
+	{
 		str = malloc(new_size);
 		free(ptr);
 		return (str);
+	}
 	if (new_size == 0 && ptr != NULL)
+	{
 		free(ptr);
 		return (NULL);
+	}
 	return (ptr);
 }
