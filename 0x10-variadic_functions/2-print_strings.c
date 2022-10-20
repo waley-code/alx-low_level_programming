b0aa53debca4da67c3ab8e8b5ef462adc3b1937a
Adeosun Oyewale <walephlp@gmail.com>
2022-10-20 15:22:30 -0700

    update README.md

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -2,25 +2,25 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 /**
-* print_strings - Main entry function Point
+* print_strings - Entry function Point
 * @separator: Operator comma space
 * @n: Argument number of elements
 * Return: Returns void
 */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list list;
+	va_list valist;
 	unsigned int i;
 	char *str;
 
 	if (separator == NULL)
 		return;
 
-	va_start(list, n);
+	va_start(valist, n);
 
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(list, char *);
+		str = va_arg(valist, char *);
 		if (str == NULL)
 		{
 			str = "(nil)";
@@ -33,5 +33,5 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	}
 	printf("\n");
 
-	va_end(list);
+	va_end(valist);
 }
--- README.md
+++ README.md
@@ -19,3 +19,17 @@
 ### 0x08-recursion
 
 ### 0x09. C - Static libraries
+
+### 0x0B-malloc_free
+
+### 0x0C-more_malloc_free
+
+### 0x0D-preprocessor
+
+### 0x0E-structures_typedef
+
+### 0x0F-function_pointers
+
+### 0x10-variadic_functions
+
+### 0x12-singly_linked_lists
