414c64a6f621049268f0822fd0059490b72dd19d
Adeosun Oyewale <walephlp@gmail.com>
2022-12-08 10:12:27 -0800

    task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_dlistint - main functio
+ * dlistint_t - Pointer datatype
+ * h - pointer
+ * Return: Always EXIT_SUCCESS.
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+
+	size_t i = 0;
+
+	if(h == 0)
+	{
+		printf("Empty list");
+
+	}
+	else{
+		while(h)
+		{
+			printf("%d\n",h->n);
+			h = h->next;
+			i++;
+		}
+	}
+	return(i);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,5 @@
+# C - Doubly linked lists
+
+### 0-print_dlistint.c
+
+	A function that returns the number of elements in a linked dlistint_t list.
