1480d495f9307a31ef7b5d6fd414238a76c8dfd7
Adeosun Oyewale <walephlp@gmail.com>
2022-12-08 10:47:51 -0800

    tast 2-9

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * add_dnodeint - check the code
+ * @head: head node
+ * @n: int argument
+ * Return: Always EXIT_SUCCESS.
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+{
+		/* declarations */
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+
+	/* check for null && malloc fail, free new if it exists, return null */
+	if (!head || !new)
+		return (new ? free(new), NULL : NULL);
+	/* assign some things to new node */
+	new->n = n;
+	new->prev = NULL;
+	/* if no other nodes, make this one the ending node as well */
+	if (!*head)
+	{
+		*head = new;
+		new->next = NULL;
+	}
+	/* otherwise, fit it in nicely */
+	else
+	{
+		new->next = *head;
+		(*head)->prev = new;
+		*head = new;
+	}
+	return (new);
+}
